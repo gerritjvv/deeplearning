231ce9806e80c751fd966cd4bea19cb43d6ee119
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2020-12-09 18:23:06 +0100

    add comments and some random weight update

--- ch4/simple_gradient_descent_1.py
+++ ch4/simple_gradient_descent_1.py
@@ -1,22 +1,35 @@
+#
+# Learning is just reducing error. You can modify the weight to reduce the error.
+#
+import random
 
 def run():
     input = 0.5
     goal = 0.8
     alpha = 0.01
-    weight = 0.5
+    weight = random.random()
 
     for i in range(20000):
         pred = weight * input
+
         error = (pred - goal) ** 2  # squared error
 
         if error < 9.603747960254256e-20:
             break
 
-        weight_delta = (pred - goal) * input
+        # calculations direction and amount from error
+        # contains pure error (pred - goal). Multiplying by the input provides scaling, negative reversal and stopping.
+        #  stopping: if input is zero the the update is zero also
+        #  negative reversal:  When input is negative, moving the weight up makes the prediction go down, its reversed.
+        #                       Multiplying by the negative input reverses the sign again. This ensures the weight
+        #                       moves in the correct direction.
+        #  scaling: Big pure errors are amplified while smaller errors are nearly ignored.
+        #
+        direction_and_amount = (pred - goal) * input
 
-        print(f"[{i}] Error {error} w_delta {weight_delta} pred {pred} weight {weight}")
+        print(f"[{i}] Error {error} w_delta {direction_and_amount} pred {pred} weight {weight}")
 
-        weight -= weight_delta * alpha
+        weight -= direction_and_amount * alpha
 
     print(f"weight {weight} pred {pred}")
 
